13f53966d942c1a7599a2df0ca53287421e49bb0
KempoMan <ajfitzgerald@gmail.com>
2017-04-19 22:39:15 -0700

    Commit before Features migration

--- Models/Model.cs
+++ Models/Model.cs
@@ -1,12 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace vega.Models
 {
+    [Table("Models")]
     public class Model
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public Make Make { get; set; }
-        public int MakeId { get; set; }
 
-        // testing
+        [Required]
+        [StringLengthAttribute(255)] 
+        public string Name { get; set; }
+        
+        public Make Make { get; set; }
+        
+        public int MakeId { get; set; }
     }
 }
